76c2d5661fdea753248ff23935d19e4de9165d5e
Adek Niyi <adekniyi@gmail.com>
2020-07-07 04:37:09 -0700

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let btn = document.getElementById('btn');
+let h2 = document.getElementById('h2')
 let input = document.getElementById('input');
 let span = document.querySelector('.output')
-let p = document.querySelector('p')
+    //let p = document.querySelector('p')
 
 
 btn.addEventListener('click', displayMessage)
@@ -9,10 +10,22 @@ btn.addEventListener('click', displayMessage)
 function displayMessage() {
     let inputValue = input.value;
     if (inputValue === "") {
-        p.style.display = 'block'
+        const message = "Please fill in the field";
+
+        createP(message)
     } else {
         // p.style.display = 'block'
-        span.textContent = inputValue
-        p.style.display = 'none'
+        span.textContent = inputValue.toUpperCase();
     }
+}
+
+function createP(message) {
+    let p = document.createElement('p')
+    p.className = "p";
+    const container = document.querySelector('.container')
+    p.appendChild(document.createTextNode(message))
+    container.insertBefore(p, h2)
+    p.style.display = 'block'
+    setTimeout(() => p.remove(), 3000)
+
 }
